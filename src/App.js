79157ea3a95207fa24570a283baad3a84b150b13
chaitanya <chandgechaitanya@gmail.com>
2023-10-21 12:49:37 +0530

    some other commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import './App.module.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './components/Home'
 
-import './App.css';
 
 function App() {
   return (
     <>
+    
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home/>}>
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
